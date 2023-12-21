5a954a438cbbd62048ed2734239bd64f6b5c04fd
Scalp_care <whsworka135@naver.com>
2023-12-21 12:20:31 +0900

    초기상태 프로젝트 테스트 푸시

---
+++ ScalpCare/src/main/java/com/Iplus/ScalpCareApplication.java
@@ -0,0 +1,13 @@
+package com.Iplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalpCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalpCareApplication.class, args);
+	}
+
+}
---
+++ ScalpCare/src/test/java/com/Iplus/ScalpCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Iplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalpCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
